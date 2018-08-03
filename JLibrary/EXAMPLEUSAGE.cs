97e9254662c16dedfd18bf6a33c5bbd22fde4829
rakion99 <_rakion99@outlook.com>
2018-08-03 00:46:45 -0500

    Uploaded Project

---
+++ JLibrary/EXAMPLEUSAGE.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using InjectionLibrary;
+
+namespace JLibrary
+{
+    class EXAMPLEUSAGE
+    {
+        public void Inject(int pID, Byte[] dllbytes)
+        {
+            InjectionMethod method = InjectionMethod.Create(InjectionMethodType.Standard); //InjectionMethodType.Standard //InjectionMethodType.ManualMap //InjectionMethodType.ThreadHijack
+            IntPtr zero = IntPtr.Zero;
+            using (PortableExecutable.PortableExecutable executable = new PortableExecutable.PortableExecutable(dllbytes))
+            {
+                zero = method.Inject(executable, pID);
+            }
+            if (zero != IntPtr.Zero)
+            {
+                //BAIL HERE - ERROR
+            }
+            else if (method.GetLastError() != null)
+            {
+                //ERROR OCCURED
+                System.Windows.Forms.MessageBox.Show(method.GetLastError().Message);
+            }
+            //SUCCESS
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # JLibrary
+
+#Made by Jason
+
+[https://www.mpgh.net/forum/showthread.php?t=531732](https://www.mpgh.net/forum/showthread.php?t=531732)
